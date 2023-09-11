696f8048e5c6647000373a7cfcb4ec00b1af049a
Sodiq 'Ade' Sanusi <ademilddiq@gmail.com>
2023-09-11 16:30:04 +0100

    Done with the Header Movie Poster (Mobile)
    Moving on to making it responsive for large screen

--- src/components/PosterDetails/index.jsx
+++ src/components/PosterDetails/index.jsx
@@ -1,11 +1,35 @@
+import { FaPlayCircle } from "react-icons/fa";
+import rottenTomatoes from "../../assets/rottenTomatoes.svg";
+import imdb from "../../assets/imdb.svg";
 import styles from "./PostDetails.module.css";
+import PropTypes from "prop-types";
 
-const PosterDetails = () => {
+const PosterDetails = ({poster_movie}) => {
+
   return ( 
-    <article className={styles.container}>
-      Poster details
+    <article className={styles.articleContainer}>
+      <h1>{poster_movie.title}</h1>
+      <div className={styles.rating}>
+        <div>
+          <div><img src={imdb} alt="IMDB rating" /></div>
+          <p>97%</p>
+        </div>
+        <div>
+          <div><img src={rottenTomatoes} alt="Rotten Tomatoes rating" /></div>
+          <p>86.0 / 100</p>
+        </div>
+      </div>
+      <p>{poster_movie.overview}</p>
+      <button className={styles.trailerButton}>
+        <FaPlayCircle />
+        Watch Trailer
+      </button>
     </article>
   );
+}
+
+PosterDetails.propTypes = {
+  poster_movie: PropTypes.object,
 }
  
 export default PosterDetails;
